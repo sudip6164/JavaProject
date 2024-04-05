14acf5fc080235153ef9221095c8ece32122ae9d
sudip6164 <sudippradhanadgj@gmail.com>
2024-04-05 20:38:00 +0545

    backup1 login_bootstrap-modal

---
+++ Backup1/AdvanceJava/src/main/java/com/advancejava/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.advancejava.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Enable web security
+public class SecurityConfig {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf().disable() // Disable csrf for simplicity (consider enabling for production)
+        .authorizeRequests()
+//          .requestMatchers("/products").hasAnyRole("CUSTOMER") // Allow customer to view products
+//          .requestMatchers("/products/**").hasRole("STAFF") // Allow staff to CRUD products
+//          .requestMatchers("/admin/**","/admindashboard/**").hasRole("ADMIN") // Admin has full access
+          .requestMatchers("/**").permitAll() // Admin has full access
+          .anyRequest().authenticated()
+        .and()
+          .formLogin().disable() // Disable default login form
+          .logout().disable()
+          .httpBasic(); // Enable HTTP Basic authentication
+
+    return http.build();
+  }
+}
